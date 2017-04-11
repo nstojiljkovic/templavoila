3870adbad0cb8892303195a39bd56ac9a641bb9d
Jenkins <jenkins@essentialdots.com>
2017-04-11 10:00:00 +0000

    [RELEASE] Release of templavoila 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,12 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "templavoila".
-#
-# Auto generated 01-12-2012 15:45
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'TemplaVoila!',
 	'description' => 'Point-and-click, popular and easy template engine for TYPO3. Public free support is provided only through TYPO3 mailing lists! Contact by e-mail for commercial support.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '2.1.0-dev',
+	'version' => '2.1.0',
 	'dependencies' => 'static_info_tables,cms,lang',
 	'conflicts' => 'kb_tv_clipboard,templavoila_cw,eu_tradvoila,me_templavoilalayout,me_templavoilalayout2',
 	'priority' => '',
@@ -32,25 +23,30 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.2.0-0.0.0',
-			'static_info_tables' => '',
-			'cms' => '',
-			'lang' => '',
-		),
-		'conflicts' => array(
-			'kb_tv_clipboard' => '-0.1.0',
-			'templavoila_cw' => '-0.1.0',
-			'eu_tradvoila' => '-0.0.2',
-			'me_templavoilalayout' => '',
-			'me_templavoilalayout2' => '',
-		),
-		'suggests' => array(
-		),
+	'constraints' => 
+	array (
+	  'depends' => 
+	  array (
+	    'php' => '5.3.0-0.0.0',
+	    'typo3' => '6.2.0-0.0.0',
+	    'static_info_tables' => '',
+	    'cms' => '',
+	    'lang' => '',
+	  ),
+	  'conflicts' => 
+	  array (
+	    'kb_tv_clipboard' => '-0.1.0',
+	    'templavoila_cw' => '-0.1.0',
+	    'eu_tradvoila' => '-0.0.2',
+	    'me_templavoilalayout' => '',
+	    'me_templavoilalayout2' => '',
+	  ),
+	  'suggests' => 
+	  array (
+	  ),
 	),
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 );
 
