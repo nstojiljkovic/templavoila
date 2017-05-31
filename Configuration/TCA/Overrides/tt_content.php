16041407e725099d92a3928a52f35f3135f60e89
Marko Ćirić <marko.ciric@essentialdots.com>
2017-05-31 17:00:23 +0200

    [BUGFIX] Fix tt_content record TCA overrides for templavoila
    Change-Id: Ia51333f123e115e384ec22b234dfbc59070f06cd
    Related: #TP-4350

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -72,21 +72,21 @@ $tempColumns = array(
 );
 t3lib_extMgm::addTCAcolumns('tt_content', $tempColumns, 1);
 
-$TCA['tt_content']['ctrl']['typeicons'][$_EXTKEY . '_pi1'] = t3lib_extMgm::extRelPath($_EXTKEY) . '/icon_fce_ce.png';
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicons'][$_EXTKEY . '_pi1'] = t3lib_extMgm::extRelPath($_EXTKEY) . '/icon_fce_ce.png';
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes'][$_EXTKEY . '_pi1'] =  'extensions-templavoila-type-fce';
 t3lib_extMgm::addPlugin(array('LLL:EXT:templavoila/locallang_db.xml:tt_content.CType_pi1', $_EXTKEY . '_pi1', 'EXT:' . $_EXTKEY . '/icon_fce_ce.png'), 'CType', $_EXTKEY);
 
 if ($_EXTCONF['enable.']['selectDataStructure']) {
-	if ($TCA['tt_content']['ctrl']['requestUpdate'] != '') {
-		$TCA['tt_content']['ctrl']['requestUpdate'] .= ',';
+	if ($GLOBALS['TCA']['tt_content']['ctrl']['requestUpdate'] != '') {
+		$GLOBALS['TCA']['tt_content']['ctrl']['requestUpdate'] .= ',';
 	}
-	$TCA['tt_content']['ctrl']['requestUpdate'] .= 'tx_templavoila_ds';
+	$GLOBALS['TCA']['tt_content']['ctrl']['requestUpdate'] .= 'tx_templavoila_ds';
 }
 
 
 if(tx_templavoila_div::convertVersionNumberToInteger(TYPO3_version) >= 4005000) {
 
-	$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] =
+	$GLOBALS['TCA']['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] =
 		'--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
 					--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.headers;headers,
 				--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
@@ -103,7 +103,7 @@ if(tx_templavoila_div::convertVersionNumberToInteger(TYPO3_version) >= 4005000) 
 	t3lib_extMgm::addToAllTCAtypes('tt_content', 'tx_templavoila_flex;;;;1-1-1', $_EXTKEY . '_pi1', 'after:subheader');
 
 } else {
-	$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] =
+	$GLOBALS['TCA']['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] =
 		'CType;;4;;1-1-1, hidden, header;;' . (($_EXTCONF['enable.']['renderFCEHeader']) ? '3' : '' ) . ';;2-2-2, linkToTop;;;;3-3-3,
 		--div--;LLL:EXT:templavoila/locallang_db.xml:tt_content.CType_pi1,' . (($_EXTCONF['enable.']['selectDataStructure']) ? 'tx_templavoila_ds,' : '') . 'tx_templavoila_to,tx_templavoila_flex;;;;2-2-2,
 		--div--;LLL:EXT:cms/locallang_tca.xml:pages.tabs.access, starttime, endtime, fe_group';
